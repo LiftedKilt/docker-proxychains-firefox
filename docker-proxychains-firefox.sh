727e75666b303eff30af9684667b6fe5f8f90b39
liftedkilt <williamdforsyth@gmail.com>
2016-08-04 13:12:14 -0700

    Update ssh settings and pull image

--- docker-proxychains-firefox.sh
+++ docker-proxychains-firefox.sh
@@ -22,8 +22,10 @@ for d in `find /dev/dri -type c` ; do
 	dri_devices+=(--device "${d}")
 done
 
+#docker pull liftedkilt/proxychains-firefox:latest
+
 nohup docker run --rm --name="proxychains-firefox" --volumes-from storage --volume ${SSH_KEY}:/id_rsa --env SOCKS_SERVER="socks://172.17.0.1:5080" --env SOCKS_VERSION=5 --env DISPLAY="${DISPLAY}" --volume /tmp/.X11-unix:/tmp/.X11-unix --env PULSE_SERVER="unix:/tmp/pulse-unix" --volume /run/user/"${UID}"/pulse/native:/tmp/pulse-unix "${dri_devices[@]}" --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro liftedkilt/proxychains-firefox:latest "$@" >/dev/null 2>&1 &
 while [[ ! $(docker ps | grep proxychains-firefox) ]]; do
 	sleep 1
 done
-docker exec -d proxychains-firefox nohup ssh -TND 12345 ${SSH_USER}@${SSH_SERVER} -i /id_rsa
+docker exec -it proxychains-firefox nohup ssh -o "StrictHostKeyChecking no" -TND 12345 ${SSH_USER}@${SSH_SERVER} -i /id_rsa
